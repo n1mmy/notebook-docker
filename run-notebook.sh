cb3141d093fed237e90e90b4dda23fa7beb5636f
Nick Martin <nim-github@nimlabs.org>
2022-08-31 14:03:24 -0700

    allow overriding ipython binary

--- run-notebook.sh
+++ run-notebook.sh
@@ -7,12 +7,14 @@ if [ -z "$NOTEBOOK" ] ; then
    exit 1
 fi
 
+IPYTHON=${IPYTHON:-ipython3}
+
 tmpfile=$(mktemp /tmp/notebook.exec.XXXXXX.py)
 jupyter nbconvert --log-level WARN --to python "$NOTEBOOK" --output "$tmpfile"
 
 cd $(dirname $NOTEBOOK)
 shift # drop notebook name from args
-ipython3 "$tmpfile" -- $@
+$IPYTHON "$tmpfile" -- $@
 exitcode=$?
 
 rm -f "$tmpfile"
